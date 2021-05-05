b6622a101cfebbe26a5e5169275f5d8b6f3c60a4
hdmzz <73470122+hdmzz@users.noreply.github.com>
2021-05-05 16:23:31 +0200

    Delete backend directory

--- backend/routes/account.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const accountCtrl = require('../controllers/account');
-
-router.get('/:id', auth, accountCtrl.getAccountInfo);
-router.delete('/:id', auth, accountCtrl.deleteAccount);
-
-module.exports = router;
